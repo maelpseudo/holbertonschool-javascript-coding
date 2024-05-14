a8276c5aef18e42a4602a0fb19bae7b6b339a8ee
maelpseudo <mael.cuny@gmail.com>
2024-05-14 13:00:06 +0200

    Added 0 task file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,27 +1,12 @@
-// Import the 'fs' module for file system operations
+#!/usr/bin/node
 const fs = require('fs');
 
-// Get the file path from the command line arguments
 const filePath = process.argv[2];
 
-// Function to read and print file content
-function readFile(filePath) {
-  fs.readFile(filePath, 'utf-8', (error, data) => {
-    if (error) {
-      // Print the error object if there's an error
-      console.error({
-        Error: error.message,
-        errno: error.errno,
-        code: error.code,
-        syscall: error.syscall,
-        path: error.path
-      });
-    } else {
-      // Print the file content if read successfully
-      console.log(data);
-    }
-  });
-}
-
-// Call the function with the provided file path
-readFile(filePath);
+fs.readFile(filePath, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
