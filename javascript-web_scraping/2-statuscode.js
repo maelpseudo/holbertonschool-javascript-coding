2bb2310fdb408103894136e0a7fb2cffd4af36c6
maelpseudo <mael.cuny@gmail.com>
2024-05-14 13:02:59 +0200

    Updated 2 files

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 
-// Retrieve the URL from the command line argument
 const url = process.argv[2];
 
-// Function to perform a GET request
-request(url, function (error, response) {
-  if (error) {
-    console.log(error);
-  } else {
-    // Print the status code
-    console.log(`code: ${response.statusCode}`);
-  }
+request.get(url).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
